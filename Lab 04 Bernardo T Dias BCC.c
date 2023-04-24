7e1073a5f68ceeeaf3582ba65108e3a88183d7e4
Bernardo Tibaldi Dias <127687867+Bernardo-Tibaldi@users.noreply.github.com>
2023-04-24 20:51:17 -0300

    Create Lab 04 Bernardo T Dias BCC.c

---
+++ Lab 04 Bernardo T Dias BCC.c
@@ -0,0 +1,60 @@
+//EXERCÍCIO 1
+
+#include <stdio.h>
+#include <math.h>
+
+int dobro (int a) {
+  return 2*a;
+}
+int main () {
+  printf ("Digite um numero:\n");
+  int a, resultado;
+  scanf ("%d", &a);
+  resultado = dobro(a);
+  printf ("O dobro desse valor eh: %d\n", resultado);
+  return 0;
+}
+
+//EXERCÍCIO 2
+
+//EXERCÍCIO 3
+
+//EXERCÍCIO 4
+
+//EXERCÍCIO 5
+
+//EXERCÍCIO 6
+
+//EXERCÍCIO 7
+
+//EXERCÍCIO 8
+
+//EXERCÍCIO 9
+
+//EXERCÍCIO 10
+
+//EXERCÍCIO 11
+
+//EXERCÍCIO 12
+
+//EXERCÍCIO 13
+
+//EXERCÍCIO 14
+
+//EXERCÍCIO 15
+
+//EXERCÍCIO 16
+
+//EXERCÍCIO 17
+
+//EXERCÍCIO 18
+
+//EXERCÍCIO 19
+
+//EXERCÍCIO 20
+
+//EXERCÍCIO 21
+
+//EXERCÍCIO 23
+
+//EXERCÍCIO 24
