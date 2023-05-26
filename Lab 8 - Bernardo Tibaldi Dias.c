b73c050321046e4513da464ef49254363d052e5b
Bernardo Tibaldi Dias <127687867+Bernardo-Tibaldi@users.noreply.github.com>
2023-05-26 10:04:24 -0300

    Create Lab 8 - Bernardo Tibaldi Dias.c

---
+++ Lab 8 - Bernardo Tibaldi Dias.c
@@ -0,0 +1,29 @@
+//EXERCÍCIO 1
+
+#include <stdio.h>
+
+int main () {
+  float V[10];
+  void *p;
+  int i;
+  p = &V[0];
+  for (i = 0; i < 10; i++) {
+    p = &V[i];
+    printf("%p\n", p);
+  }  
+  return 0;
+}
+
+//EXERCÍCIO 2
+
+//EXERCÍCIO 3
+
+//EXERCÍCIO 4
+
+//EXERCÍCIO 5
+
+//EXERCÍCIO 6
+
+//EXERCÍCIO 7
+
+//EXERCÍCIO 8
