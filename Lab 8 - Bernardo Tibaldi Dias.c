2d683d75fa047f8a83a8bc665d62ecf1a37898bf
Bernardo Tibaldi Dias <127687867+Bernardo-Tibaldi@users.noreply.github.com>
2023-05-29 19:34:47 -0300

    Update Lab 8 - Bernardo Tibaldi Dias.c

--- Lab 8 - Bernardo Tibaldi Dias.c
+++ Lab 8 - Bernardo Tibaldi Dias.c
@@ -106,6 +106,56 @@ int main () {
 }
 //EXERCÍCIO 6
 
+#include <stdio.h>
+
+int main () {
+  int V[20], valor, *p, i;
+  p = &V[0];
+  printf("Indique os valores de um array:\n");
+    scanf("%d", &*p);
+  for (i = 0; i < 4*5; i+=4) {
+    V[i] = *p;
+    printf("O valor %d eh igual a %d\n", *p, V[i]);
+          //Demonstrando com e sem o uso de índices;
+  }
+  return 0;
+}
+
 //EXERCÍCIO 7
 
+#include <stdio.h>
+
+int main () {
+  int V[10], i;
+  int *p = V;
+  printf("Indique o valor de 10 elementos:\n");
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &*(p+i));
+  }
+  int max = *p, min = *p;
+  for (i = 0; i < 10; i++) {
+    if (*(p+i) > max) {
+      max = *(p+i);
+    }
+    if (*(p+i) < min) {
+      min = *(p+i);
+    }
+  }
+  printf("O maior elemento desse array eh %d e o menor eh %d\n", max, min);
+  return 0;
+}
+
 //EXERCÍCIO 8
+
+#include <stdio.h>
+
+int main () {
+  int A, *B, **C, ***D;
+  printf("Digite um valor A:\n");
+  scanf("%d", &A);
+  B = &A;
+  C = &B;
+  D = &C;
+  printf ("Dobro: %d\nTriplo: %d\nQuadruplo: %d\n", *B*2, **C*3, ***D*4);
+  return 0;
+}
